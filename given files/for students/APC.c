6e045fbafc144500887836a3607da50526becfbd
Ofry Livney <ofry.livney@gmail.com>
2018-05-23 11:22:18 +0300

    lol

---
+++ given files/for students/APC.c
@@ -0,0 +1,29 @@
+#include "APC.h"
+
+typedef struct APC_t {
+	char* ID;
+	PSOLDIER[APC_MAX_Soldiers] soldier_roster;	
+}APC;
+
+PAPC  APC_Create(char* ID);
+
+void APC_Print(PAPC APC) {
+	if (APC == NULL) {
+		printf(ARG_ERR_MSG);
+		return;
+	}
+	printf("%s , Occupancy: %d/%d\n", APC->ID, APC->Stack_Top, APC_MAX_Soldiers);
+	if (APC->Stack_Top > 0) {
+		for (int i = 0; i < APC->Stack_Top; i++) {
+			printf("Seat %d: ", i + 1);
+			Soldier_Print(APC->Soldiers[i]);
+		}
+	}
+}
+
+
+
+
+
+
+
---
+++ given files/for students/APC.h
@@ -0,0 +1,17 @@
+#ifndef _APC_h
+#define _APC_h
+
+#include "defs.h"
+
+typedef struct APC_t APC;
+typedef struct APC_t* PAPC;
+PAPC  APC_Create(char* ID);
+void  APC_Delete(PAPC apcname);
+Result APC_Print(PAPC apcname);
+PAPC APC_Duplicate(PAPC apcname);
+Result APC_Insert_Soldier(PAPC apcname, PSOLDIER solname);
+PSOLDIER APC_Pop(PAPC apcname);
+
+
+
+#endif
